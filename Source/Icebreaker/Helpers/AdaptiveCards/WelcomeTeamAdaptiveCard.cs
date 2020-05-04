7596f012941a55eb6d89d5e0729ec2dc17fed337
Maria1004 <64522290+Maria1004@users.noreply.github.com>
2020-05-04 16:29:35 +0300

    Update WelcomeTeamAdaptiveCard.cs

--- Source/Icebreaker/Helpers/AdaptiveCards/WelcomeTeamAdaptiveCard.cs
+++ Source/Icebreaker/Helpers/AdaptiveCards/WelcomeTeamAdaptiveCard.cs
@@ -42,7 +42,7 @@ namespace Icebreaker.Helpers.AdaptiveCards
             var tourTitle = Resources.WelcomeTourTitle;
             var appId = CloudConfigurationManager.GetSetting("ManifestAppId");
             var welcomeCardImageUrl = $"https://{baseDomain}/Content/welcome-card-image.png";
-            var tourUrl = $"https://github.com/OfficeDev/microsoft-teams-icebreaker-app/wiki/images/IcebreakerScheduling.gif";
+            var tourUrl = $"https://teams.microsoft.com/l/task/{appId}?url={htmlUrl}&height=533px&width=600px&title={tourTitle}";
             var repoUrl = "https://github.com/Maria1004/meet-and-greet";
             var salutationText = Resources.SalutationTitleText;
             var tourButtonText = Resources.TakeATourButtonText;
